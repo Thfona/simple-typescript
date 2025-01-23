03bca9702bb142fb3b7db2493d76992c4af5c4eb
Thfona <thiagofn.1997@gmail.com>
2025-01-23 07:17:13 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple TypeScript
+
+Simple TypeScript starter project.
+
+## Scripts
+
+```bash
+# Install dependencies
+$ npm install
+
+# Start application
+$ npm start
+
+# Build
+npm run build
+
+# Lint
+$ npm run lint
+$ npm run lint:fix
+```
---
+++ eslint.config.mjs
@@ -0,0 +1,104 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import tsParser from '@typescript-eslint/parser';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+
+const jsFiles = [
+    '**/*.js',
+    '**/*.jsx',
+    '**/*.mjs',
+    '**/*.cjs',
+];
+
+const tsFiles = [
+    '**/*.ts',
+    '**/*.tsx',
+    '**/*.mts',
+    '**/*.cts',
+];
+
+const codeFiles = [
+    ...jsFiles,
+    ...tsFiles,
+];
+
+export default [
+    {
+        name: 'Ignores',
+        ignores: ['dist', 'node_modules', '**/.vercel', '**/package-lock.json'],
+    },
+    {
+        ...js.configs.recommended,
+        name: 'Code',
+        files: codeFiles,
+        rules: {
+            ...js.configs.recommended.rules,
+            'arrow-parens': ['error', 'always'],
+            'comma-dangle': ['error', 'always-multiline'],
+            quotes: ['error', 'single'],
+            semi: ['error', 'always'],
+        },
+    },
+    {
+        name: 'JavaScript',
+        files: jsFiles,
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+            ecmaVersion: 2022,
+            sourceType: 'module',
+        },
+    },
+    {
+        name: 'TypeScript',
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
+        },
+        files: tsFiles,
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+            parser: tsParser,
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            parserOptions: {
+                project: 'tsconfig.json',
+            },
+        },
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'error',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-non-null-assertion': 'off',
+        },
+    },
+    {
+        name: 'JSON',
+        plugins: {
+            json,
+        },
+        files: [
+            '**/*.json',
+        ],
+        language: 'json/json',
+        rules: {
+            'json/no-duplicate-keys': 'error',
+        },
+    },
+    {
+        name: 'Markdown',
+        plugins: {
+            markdown,
+        },
+        files: [
+            '**/*.md',
+        ],
+        language: 'markdown/commonmark',
+        rules: {
+            'markdown/no-html': 'error',
+        },
+    },
+];
